ec29779eb13b3fd83d05f03c555f746f501027a8
Jhancn <jhancn39@gmail.com>
2023-11-03 11:30:23 +0530

    3rd commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8000;
 const server = http.createServer((req, res) => {
           res.statusCode = 200;
           res.setHeader('Content-Type', 'text/plain');
-          res.end('Hello CG!\n');
+          res.end('Hello Mr Pavan\n');
 });
 
 server.listen(port, hostname, () => {
